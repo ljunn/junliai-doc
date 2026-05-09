fe8544eef6cb6887e4058e60da00d5ef63f8ed6b
ljpeng7 <ljpeng7@iflytek.com>
2026-05-09 12:06:19 +0800

    feat:修改位置

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+// .vitepress/config.ts
+export default {
+  themeConfig: {
+      title:"OpenApi",
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '充值和订阅', link: '/fee' }
+    ]
+  }
+}
